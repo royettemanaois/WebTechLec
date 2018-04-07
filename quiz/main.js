16025b127f53395c9829effba8dce68fde67b463
royettemanaois <35594246+royettemanaois@users.noreply.github.com>
2018-04-08 02:51:22 +0800

    Updated functionality and added nav

--- quiz/main.js
+++ quiz/main.js
@@ -3,7 +3,7 @@
 ***/
 
 
-	var questions = ["sample1","sample2","sample3"];
+	var questions = ["sample1 <br> 1","sample2","sample3"];
 	var answers =  ["answer1","ANSWER2","AnsWer3"];
 
 	var index = 1;
@@ -12,6 +12,7 @@ window.onload = function () {
 	    nextQuestion(0);
 		var userAnswer = new Array(questions.length);
 		localStorage.setItem("userAnswer",JSON.stringify(userAnswer));
+		generateQuestionNav();
 		
 }
 
@@ -19,27 +20,30 @@ function nextQuestion(x){
 
 	document.getElementById('question').innerHTML = questions[x];
 	index = x;
+	
+	
 }
 
 function previousQuestion(x){
 
 	document.getElementById('question').innerHTML = questions[x];
 	index = x;
-	changeEvents();
+	submitAnswer(x+1);
 }
-
-
-
 
 function submitAnswer(x){
 	var userAns = document.getElementById('answer').value;
 	var userAnswer = JSON.parse(localStorage.getItem("userAnswer"));
-	
 	userAnswer[x] = userAns;
-
 	localStorage.setItem("userAnswer",JSON.stringify(userAnswer));
 	document.getElementById('answer').value = "";
+	
+	if(x < questions.length){
+		updateNav(x);
+	}
+	
 	changeEvents();
+
 
 
 }
@@ -72,5 +76,33 @@ function changeEvents(){
 	document.getElementById('answer').value = userAnswer[i-1];
 	document.getElementById('nxtBtn').onclick = function () {nextQuestion(i);  submitAnswer(i-1); }
 	document.getElementById('bckBtn').onclick = function () {previousQuestion(v);   }
+
 	
+	
+}
+
+function generateQuestionNav() {
+	var d = document.getElementById('qNav');
+
+	for(var i = 0; i < questions.length; i++){
+		var newElement = "<span class='samp' id='q"+(i+1)+"' onclick='changeQuestion("+i+");'>"+(i+1)+"</span>";
+		d.innerHTML += newElement;
+	}
+}
+
+function changeQuestion(x){
+	document.getElementById('question').innerHTML = questions[x];
+	index = x;
+	changeEvents();
+}
+
+function updateNav(index){
+	var userAnswer = JSON.parse(localStorage.getItem("userAnswer"));
+
+	if(userAnswer[index] != ""){
+		document.getElementById("q"+(index+1)).style.borderTop = "8px solid green";
+	}else{
+		document.getElementById("q"+(index+1)).style.borderTop = "8px solid #1C6EA4";
+	}
+
 }
