364d5c304ff6c49f868b1738226ee7e4ef924e72
royettemanaois <35594246+royettemanaois@users.noreply.github.com>
2018-04-09 13:12:03 +0800

    Update main.js

--- HomePage/js/main.js
+++ HomePage/js/main.js
@@ -14,3 +14,24 @@ function showLearning() {
 	 document.documentElement.scrollTop = 0;
 	 document.getElementById('start1').style.display = "block";
 }
+
+function storeData(fileName, localID) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", fileName, true);
+	xhttp.setRequestHeader("Content-type", "application/json");
+		xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+		     var res = this.responseText;
+		     var string = JSON.stringify(res);
+		     localStorage.setItem(localID, string);
+		}
+	};
+	xhttp.send();
+}
+
+
+if(!localStorage.getItem("questions")){
+	storeData("questions.json","questions");
+}
+
+
